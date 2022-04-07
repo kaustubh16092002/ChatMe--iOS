d52fc6f580778f01229a6c48f0918bb5add82d92
Kaustubh Mani Tripathi <72412584+kaustubh16092002@users.noreply.github.com>
2022-04-08 00:36:58 +0530

    ChatMe made on swift using XCode for iOS

---
+++ ChatMe/Controllers/WelcomeViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+import CLTypingLabel
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = K.appName
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "⚡FlashChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+            
+//        }
+
+       
+    }
+    
+
+}
