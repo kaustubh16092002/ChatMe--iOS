dc12309ce07eedb8a27644c88a5c1aab9aca6f65
Kaustubh Mani Tripathi <72412584+kaustubh16092002@users.noreply.github.com>
2022-04-08 01:38:11 +0530

    Flashchat, a messaging app made using swit and firebase

---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,32 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Kaustubh Mani Tripathi on 31/03/22.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var rightimageView: UIImageView!
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
